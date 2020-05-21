79927c77f420ad170c605af677673f76be6dbc6f
yatharth saxena <y.s.saxena7@gmail.com>
2020-05-22 00:35:12 +0530

    Done with part1

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,96 @@
+# class PostsController < ApplicationController
+
+# 	def index
+# 		@posts = Post.all
+# 	end
+
+# 	def new
+# 		@post = Post.new
+# 	end
+
+# 	def create
+# 		@post = Post.create(post_params)
+# 		redirect_to posts_path
+# 	end
+
+# 	def show
+# 		@post = Post.find(params[:id])
+# 	end
+
+# 	def edit
+# 		@post = Post.find(params[:id])
+# 	end
+
+# 	def update
+# 		@post = Post.find(params[:id])
+# 		@post.update(post_params)
+#   		redirect_to(post_path(@post))
+# 	end
+
+# 	def destroy
+# 		@post = Post.find(params[:id])
+# 		@post.destroy
+#   		redirect_to posts_path
+# 	end
+
+# 	private
+
+# 		def post_params
+# 		  params.require(:post).permit(:image, :caption)
+# 		end
+# end
+
+class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@posts = Post.all
+		end
+
+		def show
+
+		end
+
+		def new
+		@post = Post.new
+	end
+
+	def create
+		if @post = Post.create(post_params)
+		  flash[:success] = "Your post has been created!"
+		  redirect_to posts_path
+		else
+		  flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+		  render :new
+		end
+	end
+
+	def edit
+
+	end
+
+	def update
+	    if @post.update(post_params)
+	      flash[:success] = "Post updated."
+	      redirect_to posts_path
+	    else
+	      flash.now[:alert] = "Update failed.  Please check the form."
+	      render :edit
+	    end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:image, :caption)
+	end
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
+end
