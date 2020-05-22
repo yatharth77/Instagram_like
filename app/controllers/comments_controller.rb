548cd9b787b61faf1108f16be0933443d608b56b
yatharth saxena <y.s.saxena7@gmail.com>
2020-05-22 23:32:57 +0530

    Cant run AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,46 @@
 class CommentsController < ApplicationController
 	before_action :set_post
 
-	def create
-	  @comment = @post.comments.build(comment_params)
-	  @comment.user_id = current_user.id
+	# def create
+	#   @comment = @post.comments.build(comment_params)
+	#   @comment.user_id = current_user.id
 
-	  if @comment.save
-	    flash[:success] = "You commented the hell out of that post!"
-	    redirect_back fallback_location: root_path
-	  else
-	    flash[:alert] = "Check the comment form, something went horribly wrong."
-	    render root_path
-	  end
-	end
+	#   if @comment.save
+	#     flash[:success] = "You commented the hell out of that post!"
+	#     redirect_back fallback_location: root_path
+	#   else
+	#     flash[:alert] = "Check the comment form, something went horribly wrong."
+	#     render root_path
+	#   end
+	# end
+
+	def create
+	    @comment = @post.comments.build(comment_params)
+	    @comment.user_id = current_user.id
+
+	    if @comment.save
+	      respond_to do |format|
+	        format.html { redirect_to root_path }
+	        format.js
+	      end
+	    else
+	      flash[:alert] = "Check the comment form, something went wrong."
+	      render root_path
+	    end
+	 end
 
 	def destroy
-	  @comment = @post.comments.find(params[:id])
+	    @comment = @post.comments.find(params[:id])
 
-	  @comment.destroy
-	  flash[:success] = "Comment deleted :("
-	  redirect_to root_path
+	    if @comment.user_id == current_user.id
+	      @comment.delete
+	      respond_to do |format|
+	        format.html { redirect_to root_path }
+	        format.js
+	      end
+	    end
 	end
+	
 	private
 
 	def comment_params
